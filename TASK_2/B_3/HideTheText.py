0c563f236497323ac64e88c940aaa450614d93d1
NVex0 <113530029+NVex0@users.noreply.github.com>
2023-02-15 17:00:19 +0700

    Update HideTheText.py

--- TASK_2/B_3/HideTheText.py
+++ TASK_2/B_3/HideTheText.py
@@ -1,5 +1,6 @@
 from PIL import Image
-img = Image.open("D:\Downloads\ys.png")
+imgt = Image.open("D:\Downloads\ys.png")
+img = imgt.convert("RGB")
 pix = img.load()
 cipimg = Image.new(img.mode, img.size)
 cippix = cipimg.load()
@@ -9,27 +10,33 @@ def lenbuf(val):
         val = '0' + val
     return val
 
-name = "Tran_Manh_Hung"
+txt = input("Type the text you wanna hide: ")
 cipherb = ""
-for i in name:
+for i in txt:
     cipherb += lenbuf(bin(ord(i))[2:])
 index = 0
 
 for x in range(img.size[0]):
     for y in range(img.size[1]):
-        r, g, b, a = pix[x, y]
+        r, g, b = pix[x, y]
+
+        #Bit 1 red.
         if index < len(cipherb):
             nr = lenbuf(bin(r)[2:])
             nr = int(nr[:6] + cipherb[index] + nr[-1:], 2)
             index += 1
         else:
             nr = r
+        
+        #Bit 0 green.
         if index < len(cipherb): 
             ng = lenbuf(bin(g)[2:])
             ng = int(ng[:7] + cipherb[index], 2)
             index += 1
         else:
             ng = g
+
+        #Bit 2 blue.
         if index < len(cipherb):
             nb = lenbuf(bin(b)[2:])
             nb = int(nb[:5] + cipherb[index] + nb[-2:], 2)
@@ -39,3 +46,4 @@ for x in range(img.size[0]):
         cippix[x, y] = nr, ng, nb
 
 cipimg.save("D:\Downloads\ys2nd.png")
+print("Complete.")
